b696132282057b25d86c8067378aa34f4744eced
Igor Oliveira Silva <65821168+igorosilva@users.noreply.github.com>
2021-10-17 13:57:56 -0300

    Estruturas de repetição

---
+++ aulas/aula13/ambiente.js
@@ -0,0 +1,14 @@
+var i = 0;
+
+while (i < 6) {
+    console.log(`While[${i}]`);
+    i++;
+}
+
+console.log('-------------------')
+
+i = 0
+do {
+    console.log(`Do while[${i}]`)
+    i++;
+} while (i < 6);
---
+++ aulas/aula14/ambiente.js
@@ -0,0 +1,7 @@
+var i;
+
+console.log('Vai começar...');
+for (i = 0; i < 5; i++) {
+    console.log(`For[${i}]`);
+}
+console.log('Fim!');
