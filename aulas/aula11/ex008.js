9f4d02866a1092bb4afa6507b2e1b096a2e01397
Igor Oliveira Silva <65821168+igorosilva@users.noreply.github.com>
2021-10-17 01:02:38 -0300

    Condições pt.1

---
+++ aulas/aula11/ex008.js
@@ -0,0 +1,9 @@
+var vel = 5;
+
+console.log(`A velocidade do seu carro é: ${vel}`);
+
+if (vel > 60) {
+    console.log('Você ultrapaassou a velocidade permitida. MULTADO!');
+}
+
+console.log('Dirija sempre usando cinto de segurnça!');
